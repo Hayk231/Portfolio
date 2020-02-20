0e093758ba32a94d18153e80b001c5763f573da6
Hayk231 <hayk.hovsepyan98@gmail.com>
2020-02-20 14:22:40 +0400

    Updates

---
+++ precache-manifest.2c3086de04905d0dbeda937315c900dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "313bec7d1a5992273baa6e6be7563efd",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "5054304709d01a352cf6",
+    "url": "/Portfolio/static/css/main.a75c741e.chunk.css"
+  },
+  {
+    "revision": "a802aefab9ca314d5cc8",
+    "url": "/Portfolio/static/js/2.da86e669.chunk.js"
+  },
+  {
+    "revision": "5054304709d01a352cf6",
+    "url": "/Portfolio/static/js/main.38f37b29.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "5f03c54cb165e82c131fa6f1319f1191",
+    "url": "/Portfolio/static/media/blog.5f03c54c.jpg"
+  },
+  {
+    "revision": "d9626f0ce4cfcbbd2f5b6d1c4a061a3b",
+    "url": "/Portfolio/static/media/burger.d9626f0c.jpg"
+  },
+  {
+    "revision": "8de3938be0319283a299e221212d6f70",
+    "url": "/Portfolio/static/media/recipes.8de3938b.jpg"
+  }
+]);
